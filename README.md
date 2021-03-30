5fc331b3fb6853b536753ceb1650993469122a5d
BySlin <2292735470@qq.com>
2021-03-30 11:39:24 +0800

    fix

---
+++ README.md
@@ -0,0 +1,4 @@
+禁用WebView SSL校验
+
+在config.xml中添加<br/>
+`<preference name="webview" value="org.apache.cordova.nossl.webview.NoSSLSystemWebViewEngine"/>`
---
+++ src/android/NoSSLSystemWebView.java
@@ -0,0 +1,12 @@
+package org.apache.cordova.nossl.webview;
+
+import android.content.Context;
+
+import org.apache.cordova.engine.SystemWebView;
+
+public class NoSSLSystemWebView extends SystemWebView {
+
+    public NoSSLSystemWebView(Context context) {
+        super(context);
+    }
+}
